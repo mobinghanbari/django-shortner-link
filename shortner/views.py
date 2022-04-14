b414e042a32a5eeb6247acd3fab0e1c173f24195
mobingh <mobinghanbari111@gmail.com>
2022-04-14 15:30:40 +0430

    the create view was created successfully

--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Url
+from uuid import uuid4
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
