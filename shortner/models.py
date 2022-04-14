82b0be455a28bb4284b2f1afc699dc8b84c55a7b
mobingh <mobinghanbari111@gmail.com>
2022-04-14 14:59:57 +0430

    the Url model was created successfully

--- shortner/models.py
+++ shortner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+    link = models.CharField(max_length=10000, unique=True)
+    uid = models.CharField(max_length=15, unique=True)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.link
