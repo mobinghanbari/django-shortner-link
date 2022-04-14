79e90047c6752e0e95f160486bce8db1079ca9be
mobingh <mobinghanbari111@gmail.com>
2022-04-14 15:29:36 +0430

    the model's uid field name was changed to uuid

--- shortner/models.py
+++ shortner/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Url(models.Model):
     link = models.CharField(max_length=10000, unique=True)
-    uid = models.CharField(max_length=15, unique=True)
+    uuid = models.CharField(max_length=15, unique=True)
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
